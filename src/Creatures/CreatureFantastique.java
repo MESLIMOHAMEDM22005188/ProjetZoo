e0d2cfd370f0f4a1b48789eff9569e3733a1504e
Mohamed MIESLI <mohamed.mesli@etu.univ-amu.fr>
2023-12-03 22:40:16 +0100

    Ajout de la documentation JavaDo

--- src/Creatures/CreatureFantastique.java
+++ src/Creatures/CreatureFantastique.java
@@ -3,7 +3,10 @@ package src.Creatures;
 import java.util.ArrayList;
 import java.util.List;
 
-// Interface définissant les actions disponibles pour une créature
+/**
+ * Interface définissant les actions disponibles pour une créature
+ * @author d21218703
+  */
 interface ActionsCreature {
     void manger();
     void emettreSon();
@@ -12,35 +15,83 @@ interface ActionsCreature {
     void seReveiller();
     void vieillir();
 }
+/**
+ * Enumérations pour la catégorie d'âge
+ * @author d21218703
+ */
 
-// Enumérations pour la catégorie d'âge
 enum CategorieAge {
     JEUNE, ADULTE, VIEUX, MORT
 }
-// Enumérations des noms des espèces
-enum NomEspece {
-    DRAGONS, KRAKENS, LICORNES, LYCANTHROPES, MEGALODONS, NYMPHES, PHENIX, SIRENES
-}
-// Classe abstraite représentant les créatures fantastiques
-
+/**
+ * Classe abstraite représentant les créatures fantastiques.
+ *  @author b22025963 d21218703 m22005188
+ */
 public abstract class CreatureFantastique {
-    private static CreatureFantastique CreatureFantastique = new ArrayList<>(); // Champ statique pour stocker toutes les licornes créées
+    /**
+     * Liste de créatures fantastiques.
+     */
+    public static List<CreatureFantastique> Creature = new ArrayList<>();
 
-    // ... (attributs de la créature)
-
+    /**
+     * Nom de la créature.
+     */
     protected String nom;
+
+    /**
+     * Sexe de la créature.
+     */
     protected char sexe;
+
+    /**
+     * Poids de la créature.
+     */
     protected double poids;
+
+    /**
+     * Taille de la créature.
+     */
     protected double taille;
+
+    /**
+     * Âge de la créature.
+     */
     protected int age;
+
+    /**
+     * Indicateur de faim de la créature.
+     */
     protected int indicateurFaim;
+
+    /**
+     * Indique si la créature dort.
+     */
     protected boolean dort;
+
+    /**
+     * Indicateur de santé de la créature.
+     */
     protected int indicateurSante;
 
+    // ... (autres attributs et méthodes de la classe)
+
+    /**
+     * Indicateur de sommeil de la créature.
+     */
     protected int indicateurSommeil;
 
-    // Constructeur de la classe CreatureFantastique
-    public CreatureFantastique(String nom, char sexe, double poids11, double poids1, double v, int i, double poids, double taille, int age) {
+    /**
+     * Constructeur de la classe CreatureFantastique.
+     * @param 'nomEspece' Le nom de l'espèce de la créature.
+     * @param 'sexe' Le sexe de la créature.
+     * @param 'poids' Le poids de la créature.
+     * @param 'taille' La taille de la créature.
+     * @param 'age' L'âge de la créature.
+     * @param 'indicateurFaim L'indicateur de faim de la créature.
+     * @param 'indicateurSommeil' L'indicateur de sommeil de la créature.
+     * @param 'indicateurSante' L'indicateur de santé de la créature.
+     */
+    public CreatureFantastique(String nomEspece, char sexe, double v, double poids, double taille, int age, int indicateurFaim, int indicateurSommeil, int indicateurSante) {
         this.nom = nom;
         this.sexe = sexe;
         this.poids = poids;
@@ -51,12 +102,24 @@ public abstract class CreatureFantastique {
         this.indicateurSante = 100;
         this.indicateurSommeil = 0; // Initialise à 0 par défaut
     }
+    /**
+     * Méthode statique pour créer une nouvelle créature.
+     * @param especeCreature L'espèce de la nouvelle créature.
+     * @param nomCreature Le nom de la nouvelle créature.
+     * @return La nouvelle créature fantastique créée.
+     */
+    public static CreatureFantastique creerNouvelleCreature(NomEspece especeCreature, String nomCreature) {
+        return null;
+    }
 
     public String getNom() {
         return nom;
     }
-    // Méthodes de la créature pour manger, mettre à jour la santé, etc.
-
+    /**
+     * Méthode permettant à la créature de se nourrir.
+     * La créature mangera seulement si elle n'est pas malade et si elle n'est pas en train de dormir.
+     * Sinon, un message approprié sera affiché.
+     */
     public void manger() {
         boolean isMalade = false;
         boolean estEnTrainDeDormir = indicateurSommeil == 1;
@@ -70,17 +133,33 @@ public abstract class CreatureFantastique {
         }
     }
 
+    /**
+     * Met à jour la santé de la créature en fonction de la propreté.
+     * Cette méthode est à implémenter dans les sous-classes pour définir
+     * le comportement spécifique de mise à jour de la santé basée sur la propreté.
+     */
     public void updateSanteFromProprete() {
-    }
+        // À implémenter
 
+    }
+    /**
+     * Obtient le nom de l'espèce de la créature.
+     * @return false car cette méthode devrait normalement retourner une chaîne de caractères représentant le nom de l'espèce.
+     */
     public boolean getNomEspece() {
         return false;
     }
 
+    /**
+     * Méthode abstraite à implémenter dans les sous-classes pour définir l'indicateur de propreté.
+     * @param i La valeur à utiliser pour définir l'indicateur de propreté.
+     */
     public abstract void setIndicateurProprete(int i);
 
 
-    // Interface pour la capacité de courir
+    /**
+     * Interface pour la capacité de courir.
+     */
     public interface Run {
         default void canRun(boolean hasLegs) {
             if (hasLegs) {
@@ -91,7 +170,9 @@ public abstract class CreatureFantastique {
         }
     }
 
-    // Interface pour la capacité de nager
+    /**
+     * Interface pour la capacité de nager.
+     */
     public interface CanSwim {
         default void canSwim(boolean hasFins) {
             if (hasFins) {
@@ -102,7 +183,9 @@ public abstract class CreatureFantastique {
         }
     }
 
-    // Interface pour la capacité de voler
+    /**
+     * Interface pour la capacité de voler.
+     */
     public interface CanFly {
         default void canFly(boolean hasWings) {
             if (hasWings) {
@@ -112,7 +195,12 @@ public abstract class CreatureFantastique {
             }
         }
     }
-
+    /**
+     * Méthode permettant à une créature de mettant bas.
+     * Vérifie si la créature est de type Vivipaire et de sexe féminin ('F').
+     * Si tel est le cas, la créature peut mettre bas en appelant la méthode appropriée.
+     * Sinon, un message est affiché indiquant que la créature ne peut pas mettre bas.
+     */
     public void pandre() {
         if (this instanceof Vivipaire && sexe == 'F') {
             Vivipaire creatureVivipaire = (Vivipaire) this;
@@ -122,6 +210,10 @@ public abstract class CreatureFantastique {
         }
     }
 
+    /**
+     * Méthode permettant à une créature Vivipaire de mettre bas.
+     * Cette méthode est à implémenter dans les classes qui implémentent l'interface Vivipaire.
+     */
     public void mettreBas() {
         if (this instanceof Vivipaire && sexe == 'F') {
             Vivipaire creatureVivipaire = (Vivipaire) this;
@@ -131,14 +223,30 @@ public abstract class CreatureFantastique {
         }
     }
 
+    /**
+     * Interface définissant la capacité de pandre pour une créature.
+     */
     public interface Ovipaire {
+        /**
+         * Méthode définissant la capacité de pandre pour une créature ovipaire.
+         */
         void canPandre();
     }
 
+    /**
+     * Interface définissant la capacité de mettre bas pour une créature vivipaire.
+     */
     public interface Vivipaire {
+        /**
+         * Méthode définissant la capacité de mettre bas pour une créature vivipaire.
+         */
         void canMettreBas();
     }
-
+    /**
+     * Méthode permettant à la créature de s'endormir.
+     * Si l'indicateur de sommeil est déjà à 1, affiche un message indiquant que la créature est déjà endormie.
+     * Sinon, met l'indicateur de sommeil à 1 et affiche un message indiquant que la créature s'endort.
+     */
     public void sEndormir() {
         if (indicateurSommeil == 1) {
             System.out.println(nom + " est déjà endormi(e).");
@@ -147,11 +255,11 @@ public abstract class CreatureFantastique {
             System.out.println(nom + " s'endort.");
         }
     }
-
-    public abstract void emettreSon();
-
-    public abstract void soigner();
-
+    /**
+     * Méthode permettant à la créature de vieillir d'une unité.
+     * Incrémente l'âge de la créature et affiche un message indiquant son nouvel âge.
+     * La logique de vieillissement pour différentes espèces doit être complétée dans les classes filles.
+     */
     public void vieillir() {
         age++;
         System.out.println(nom + " vieillit. Âge actuel : " + age);
@@ -218,28 +326,42 @@ public abstract class CreatureFantastique {
         this.indicateurSante = indicateurSante;
     }
 }
-
+/**
+ * Classe abstraite représentant les différentes créatures et implémentant l'interface ActionsCreature.
+ */
 abstract class Creatures implements ActionsCreature {
     protected String nomEspece;
 
     protected int phase = 1;
-
+    /**
+     * Getter pour récupérer le nom de l'espèce de la créature.
+     * @return Le nom de l'espèce de la créature.
+     */
+    public String getNomEspece() {
+        return nomEspece;
+    }
+    /**
+     * Constructeur pour initialiser le nom de l'espèce de la créature.
+     */
     public Creatures(String nomEspece) {
         this.nomEspece = nomEspece;
         // ... (initialisation)
     }
 
-    public String getNomEspece() {
-        return nomEspece;
-    }
-
-    protected abstract void setIndicateurFaim(int i);
-
+    /**
+     * Méthode pour permettre à la créature d'émettre un son.
+     * Affiche un message indiquant que la créature émet un son spécifique à son espèce.
+     */
     @Override
     public void emettreSon() {
         System.out.println(nomEspece + " émet un son.");
     }
-
+    /**
+     * Méthode pour soigner la créature.
+     * Affiche un message indiquant que la créature a été soignée, puis met à jour l'état de santé de la créature.
+     * La méthode setIsMalade(false) indique que la créature n'est plus malade.
+     * La méthode setIndicateurSante(10) met à jour l'indicateur de santé de la créature.
+     */
     @Override
     public void soigner() {
         System.out.println(nomEspece + " a été soigné.");
@@ -247,22 +369,17 @@ abstract class Creatures implements ActionsCreature {
         setIndicateurSante(10);
     }
 
+    /**
+     * Méthode abstraite à implémenter dans les sous-classes pour définir l'indicateur de santé de la créature.
+     * @param i La valeur de l'indicateur de santé à définir.
+     */
     protected abstract void setIndicateurSante(int i);
 
+    /**
+     * Méthode abstraite à implémenter dans les sous-classes pour définir si la créature est malade.
+     * @param b Valeur booléenne indiquant si la créature est malade ou non.
+     */
     protected abstract void setIsMalade(boolean b);
 
-    @Override
-    public void vieillir() {
-        CategorieAge categorieAge = CategorieAge.JEUNE;
-        if (categorieAge != CategorieAge.MORT) {
-            int age = 0;
-            age++;
-            try {
-                NomEspece espece = NomEspece.valueOf(nomEspece.toUpperCase());
-                // Logique de catégorisation par âge pour différentes espèces (à compléter)
-            } catch (IllegalArgumentException e) {
-                System.err.println("Nom d'espèce erroné !");
-            }
-        }
-    }
+
 }
