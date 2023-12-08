b099ebe76c3a143e863846aaf401aae806777a36
Mohamed MIESLI <mohamed.mesli@etu.univ-amu.fr>
2023-12-08 21:51:07 +0100

    Test Unitaire du fichier PhenixTest

---
+++ src/Creatures/PhenixTest.java
@@ -0,0 +1,52 @@
+package src.Creatures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PhenixTest {
+
+    @Test
+    void creerPhenix() {
+    }
+
+    @Test
+    void getPhenixes() {
+    }
+
+    @Test
+    void seReproduire() {
+    }
+
+    @Test
+    void emettreSon() {
+    }
+
+    @Test
+    void soigner() {
+    }
+
+    @Test
+    void getEspecesVolantes() {
+    }
+
+    @Test
+    void setEspecesVolantes() {
+    }
+
+    @Test
+    void creerNouvelleCreature() {
+    }
+
+    @Test
+    void mourirImo() {
+    }
+
+    @Test
+    void naissance() {
+    }
+
+    @Test
+    void getStade() {
+    }
+}
