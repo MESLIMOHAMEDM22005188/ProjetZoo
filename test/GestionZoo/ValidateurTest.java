071f630f5c3b056d5a480246e445c09745f04cb8
Mohamed MIESLI <mohamed.mesli@etu.univ-amu.fr>
2023-12-08 21:05:18 +0100

    Test Unitaire du fichier Validateur

---
+++ test/GestionZoo/ValidateurTest.java
@@ -0,0 +1,24 @@
+package test.GestionZoo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateurTest {
+
+    @Test
+    void saisirInfoValide() {
+    }
+
+    @Test
+    void saisirAge() {
+    }
+
+    @Test
+    void estPrenomValide() {
+    }
+
+    @Test
+    void estUniquementAlphabetique() {
+    }
+}
